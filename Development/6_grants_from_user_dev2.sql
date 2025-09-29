4352a331a87d9eda49d9e1ec7c1e58f8b52b6b98
DayronCasas <dayron.casas.sanchez@vallegrande.edu.pe>
2025-09-29 10:33:06 -0500

    Update 6_grants_from_user_dev2.sql

--- Development/6_grants_from_user_dev2.sql
+++ Development/6_grants_from_user_dev2.sql
@@ -5,3 +5,11 @@ GRANT SELECT ON orders TO DEVELOPER_01;
 GRANT SELECT ON order_details TO DEVELOPER_01;
 GRANT SELECT ON product TO DEVELOPER_01;
 GRANT SELECT ON status_tracking TO DEVELOPER_01;
+
+-------------------------------------------------
+-- PERMISOS DE DEVELOPER_02 A DEVELOPER_03
+-------------------------------------------------
+GRANT SELECT ON orders TO DEVELOPER_03;
+GRANT SELECT ON order_details TO DEVELOPER_03;
+GRANT SELECT ON product TO DEVELOPER_03;
+GRANT SELECT ON status_tracking TO DEVELOPER_03;
