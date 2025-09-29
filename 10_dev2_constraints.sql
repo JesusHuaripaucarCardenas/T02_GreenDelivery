87ab412f74e1efa578f552e99692dde399511cc4
Jesus Huaripaucar <jesus.huaripaucar@vallegrande.edu.pe>
2025-09-29 10:19:22 -0500

    Delete 10_dev2_constraints.sql

--- 10_dev2_constraints.sql
+++
@@ -1,9 +0,0 @@
-
--- Relación users -> role
-ALTER TABLE users 
-ADD CONSTRAINT fk_users_role 
-FOREIGN KEY (role_id) REFERENCES role(id);
-
-ALTER TABLE address 
-ADD CONSTRAINT fk_address_orders 
-FOREIGN KEY (orders_id) REFERENCES orders(id);
